f832e90dd444b0a6e1549dccf607121b5a768626
JeremyChowPrograms <mastermi@Jeremys-MacBook-Pro.local>
2022-02-04 01:23:44 -0800

    Setup and trying youtube-dl

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import cv2
+import torch
+from torch import nn
+
+
---
+++ vid.py
@@ -0,0 +1,11 @@
+import os
+
+url_to_path = {'https://www.youtube.com/watch?v=VKMw2it8dQY' : 'vid_data/yooh_vid.mp4'}
+
+def download_video(url, location):
+    os.system(f'youtube-dl {url} -o {location} -f mp4')
+
+for url in url_to_path:
+    download_video(url, url_to_path[url])
+
+# download_video(yooh_url, 'vid_data/yooh_vid.mp4')
