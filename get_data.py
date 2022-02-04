790eb8b95b5514db2f8fa9f93e528d985c506180
jason z <jazhang00@gmail.com>
2022-02-04 02:32:35 -0800

    more splicing examples

--- get_data.py
+++ get_data.py
@@ -1,6 +1,9 @@
 import os
 
-url_to_path = {'https://www.youtube.com/watch?v=VKMw2it8dQY' : 'vid_data/yooh_vid.mp4'}
+url_to_path = {
+    'https://www.youtube.com/watch?v=VKMw2it8dQY' : 'vid_data/yooh_vid.mp4',
+    'https://www.youtube.com/watch?v=TWKr4R8k9Ew' : 'vid_data/prep_vid.mp4'
+}
 
 def download_video(url, location):
     os.system(f'youtube-dl {url} -o {location} -f mp4')
--- vid_splice.py
+++ vid_splice.py
@@ -1,7 +1,7 @@
 import numpy as np
 import skvideo.io
 
-fp = "vid_data/yooh_vid.mp4"
+fp = "vid_data/prep_vid.mp4"
 vid = skvideo.io.vread(fp)
 T, M, N, C = vid.shape
 print("Number of frames: %d" % (T,))
@@ -9,9 +9,14 @@ print("Number of rows: %d" % (M,))
 print("Number of cols: %d" % (N,))
 print("Number of channels: %d" % (C,))
 
-print("Frame 1 mean value: ", np.mean(vid[0,:,:]))
-print("Frame 1 max value: ", np.max(vid[0,:,:]))
+print("Frame 0 mean value: ", np.mean(vid[0,:,:]))
+print("Frame 0 max value: ", np.max(vid[0,:,:]))
 
-print("Writing Frame 1 to PNG file...")
+print("Writing Frame 0 and last frame to PNG file...")
 
-skvideo.io.vwrite("frame1.png", vid[-1])
+skvideo.io.vwrite("frame0.png", vid[0])
+skvideo.io.vwrite("framelast.png", vid[-1])
+
+print("Getting a chunk of the image...")
+
+skvideo.io.vwrite("chunk.png", vid[-1: int(M/4):int(3*M/4), int(N/4):int(3*N/4), :])
