1d2daafe4d94e265f7340d89749268552da5aece
JeremyChowPrograms <mastermi@Jeremys-MacBook-Pro.local>
2022-02-10 02:42:53 -0800

    Model Sample

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ import torch
 from torch import nn
 
 
+class Img2Cov(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 16, 3)
+        self.conv2 = nn.Conv2d(16, 64, 3)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(64 * 3 * 3, 120)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square, you can specify with a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
